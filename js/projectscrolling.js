a49bf15b7db7efb7d40b115010dde38c0b7d3bec
Michal Gloncak <michalgloncak26@gmail.com>
2024-02-13 23:35:24 +0100

    first commit

---
+++ js/projectscrolling.js
@@ -0,0 +1,36 @@
+projects = document.getElementsByClassName('project');
+
+var move = 45;
+var moveChange = move;
+var project = document.getElementById('projects');
+project.style.transform = `translateY(${move}vh)`;
+
+
+var index = 0;
+var transitionFinished = true;
+
+addEventListener("wheel", (e) => {});
+onwheel = (e) => {
+    console.log("deltaY: " + e.deltaY + ", index: " + index + ", transitionFinished: " + transitionFinished);
+    projects[index].classList.remove('active');
+    if(transitionFinished == true){
+        if(e.deltaY == 100 && index != 2){
+            // scrolled down
+            moveChange -= move;
+            index ++;
+            transitionFinished = false;
+        }else if(e.deltaY == -100 && index != 0){
+            // scrolled up
+            moveChange += move;
+            index --;
+            transitionFinished = false;
+        }
+        project.style.transform = `translateY(${moveChange}vh)`;
+    }
+    console.log(index);
+    projects[index].classList.add('active');
+};
+
+project.addEventListener('transitionend', function(event) {
+    transitionFinished = true;
+}, false );
